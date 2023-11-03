8a200d7ec1895725875df8e1572a16c75e2e479b
simoneandreagrande <simoneandreagrande@gmail.com>
2023-11-03 09:50:29 +0100

    Commit

--- src/main/java/it/polito/tdp/exam/FXMLController.java
+++ src/main/java/it/polito/tdp/exam/FXMLController.java
@@ -5,8 +5,13 @@
 package it.polito.tdp.exam;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.Set;
 
+import org.jgrapht.graph.DefaultWeightedEdge;
+
+import it.polito.tdp.exam.model.Dettaglio;
 import it.polito.tdp.exam.model.Model;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -35,10 +40,10 @@ public class FXMLController {
     private Button btnSimula; // Value injected by FXMLLoader
 
     @FXML // fx:id="cmbAnno"
-    private ComboBox<?> cmbAnno; // Value injected by FXMLLoader
+    private ComboBox<Integer> cmbAnno; // Value injected by FXMLLoader
 
     @FXML // fx:id="cmbSquadra"
-    private ComboBox<?> cmbSquadra; // Value injected by FXMLLoader
+    private ComboBox<String> cmbSquadra; // Value injected by FXMLLoader
 
     @FXML // fx:id="txtResult"
     private TextArea txtResult; // Value injected by FXMLLoader
@@ -48,12 +53,46 @@ public class FXMLController {
 
     @FXML
     void handleCreaGrafo(ActionEvent event) {
-
+    	
+    	String team = cmbSquadra.getValue();
+    	this.model.creaGrafo(team);
+    	
+    	Set<Integer> vertici = this.model.getVertici();
+    	Set<DefaultWeightedEdge> archi = this.model.getArchi();
+    	
+    	this.txtResult.setText("Grafo creato.\n");
+    	this.txtResult.appendText("Ci sono " + vertici.size() + " vertici\n");
+    	this.txtResult.appendText("Ci sono " + archi.size() + " archi\n\n");
+    
+    
+    	this.cmbAnno.getItems().clear();
+    	this.cmbAnno.getItems().addAll(vertici);
+    
     }
 
     @FXML
     void handleDettagli(ActionEvent event) {
-
+    	Integer anno = this.cmbAnno.getValue();
+    	if (anno == null) {
+    		this.txtResult.setText("Selezionare un anno dall'apposito menu\n");
+    		return;
+    	}
+    	
+    	Set<DefaultWeightedEdge> archi = this.model.getArchi();
+    	
+    	if (archi.size()==0) {
+    		this.txtResult.appendText("Non ci sono archi da poter analizzare.");
+    	} else {
+    		List<Dettaglio> dettagli = this.model.getDettagli(anno);
+        	this.txtResult.appendText("\nDettagli per l'anno scelto:\n");
+        	for(Dettaglio d : dettagli) {
+        		this.txtResult.appendText("anno: " + anno+"<->"+d + "\n");
+        	}
+    	}
+    	
+    	
+ 
+    	
     }
 
     @FXML
@@ -75,6 +114,9 @@ public class FXMLController {
 
     public void setModel(Model model) {
         this.model = model;
+//        List<Team> teams = model.readAllTeams();
+    	this.cmbSquadra.getItems().setAll(model.getTeamsNames());
+//    	cmbAnno.getTeams().addAll(teams) ;
     }
 
 }
--- src/main/java/it/polito/tdp/exam/model/Model.java
+++ src/main/java/it/polito/tdp/exam/model/Model.java
@@ -1,4 +1,97 @@
 package it.polito.tdp.exam.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
+
+import it.polito.tdp.exam.db.BaseballDAO;
+
+
+
 public class Model {
+	
+	private BaseballDAO dao;
+	private Graph<Integer, DefaultWeightedEdge> grafo ;
+	private Map<Integer, List<People>> annoToPlayers;
+
+	
+	public Model() {
+		this.dao = new BaseballDAO();
+		this.annoToPlayers = new HashMap<Integer, List<People>>();
+		
+	}
+	
+	
+	public List<String> getTeamsNames() {
+		return this.dao.getTeamsNames();
+	}
+	
+	
+	
+	public void creaGrafo(String name) {
+		BaseballDAO dao = new BaseballDAO() ;
+		
+		
+		this.grafo = new SimpleWeightedGraph<Integer, DefaultWeightedEdge>(DefaultWeightedEdge.class) ;
+		
+		
+		// al grafo aggiungo i vertici
+		List<Integer> vertici = this.dao.getVertici(name);
+		Graphs.addAllVertices(this.grafo, vertici) ;
+		
+		
+		//Leggere i giocatori per ogni anno
+				
+		
+		this.annoToPlayers.clear();
+		for (int anno : vertici) {
+			this.annoToPlayers.put(anno, this.dao.getPlayersTeamYear(name, anno) );
+		}
+				
+				
+		//verificare ogni coppia di anni, e creare un arco con il peso corrispondente
+		for (int i = 0; i <vertici.size(); i++) {
+			for (int j = i+1; j < vertici.size(); j++) {
+				List<People> giocatori1 = new ArrayList<People>(this.annoToPlayers.get(vertici.get(i)));
+				List<People> giocatori2 = this.annoToPlayers.get(vertici.get(j));
+				giocatori1.retainAll(giocatori2);
+				int peso = giocatori1.size();
+				Graphs.addEdgeWithVertices(this.grafo, vertici.get(i), vertici.get(j), peso);
+			}
+		}
+		
+	}
+	
+	
+	public Set<Integer> getVertici() {
+		return this.grafo.vertexSet();
+	}
+	
+	public Set<DefaultWeightedEdge> getArchi() {
+		return this.grafo.edgeSet();
+	}
+	
+	
+	public List<Dettaglio> getDettagli(int anno) {
+		List<Dettaglio> result = new ArrayList<Dettaglio>();
+		List<Integer> adiacenti = Graphs.neighborListOf(this.grafo, anno);
+		
+		
+		for(Integer nodo : adiacenti) {
+			DefaultWeightedEdge arco = this.grafo.getEdge(anno, nodo);
+			result.add(new Dettaglio(nodo, (int)this.grafo.getEdgeWeight(arco)) );
+		}
+		return result;
+	}
+
+
 }
